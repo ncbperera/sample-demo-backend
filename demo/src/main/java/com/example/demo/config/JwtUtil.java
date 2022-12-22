193219ce9f786ffe6abc5beec3ad378c0cf23025
ncbperera <ncbperera@gmail.com>
2022-12-23 01:38:53 +0530

    initial
    initial

---
+++ demo/src/main/java/com/example/demo/config/JwtUtil.java
@@ -0,0 +1,89 @@
+package com.example.demo.config;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+@Service
+public class JwtUtil {
+
+	private String secret;
+	private int jwtExpirationInMs;
+
+	@Value("${jwt.secret}")
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
+	@Value("${jwt.expirationDateInMs}")
+	public void setJwtExpirationInMs(int jwtExpirationInMs) {
+		this.jwtExpirationInMs = jwtExpirationInMs;
+	}
+
+	public String generateToken(UserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+
+		Collection<? extends GrantedAuthority> roles = userDetails.getAuthorities();
+
+		if (roles.contains(new SimpleGrantedAuthority("ROLE_ADMIN")) || roles.contains(new SimpleGrantedAuthority("ROLE_ALLOW_EDIT"))) {
+			claims.put("isAdmin", true);
+			claims.put("role", "ROLE_ADMIN,ROLE_ALLOW_EDIT");
+		}
+		if (roles.contains(new SimpleGrantedAuthority("ROLE_USER"))) {
+			claims.put("isUser", true);
+			claims.put("role", "ROLE_USER");
+		}
+		
+		return doGenerateToken(claims, userDetails.getUsername());
+	}
+
+	private String doGenerateToken(Map<String, Object> claims, String subject) {
+		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
+				.signWith(SignatureAlgorithm.HS512, secret).compact();
+
+	}
+
+	public boolean validateToken(String authToken) {
+		try {
+			Jwts.parser().setSigningKey(secret).parseClaimsJws(authToken);
+			return true;
+		} catch (MalformedJwtException | UnsupportedJwtException | IllegalArgumentException ex) {
+			throw new BadCredentialsException("INVALID_CREDENTIALS", ex);
+		} catch (ExpiredJwtException ex) {
+			throw ex;
+		}
+	}
+
+	public String getUsernameFromToken(String token) {
+		Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+		return claims.getSubject();
+
+	}
+
+	public List<SimpleGrantedAuthority> getRolesFromToken(String token) {
+		Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+		List<SimpleGrantedAuthority> roles = null;
+		roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN,ROLE_ALLOW_EDIT"));
+		return roles;
+
+	}
+
+}
